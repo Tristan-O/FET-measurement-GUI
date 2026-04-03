6140f3e778211bd5010228003a6a520eea9190fb
tpo <troneill2015@gmail.com>
2026-04-03 12:22:07 -0700

    Cleaning up NotesInstrument

--- NotesInstrument.py
+++ NotesInstrument.py
@@ -7,11 +7,8 @@ class NotesInstrument(InstrumentBase):
 
     def __init__(self):
         super().__init__()
-        self.idn = "NOTES"
-        self.status = "open"
         self.text = ""
-    def open(self, address=None, timeout=5):
-        self.status = "open"
+    def open(self, *args, **kwargs):
         return True
     def close(self):
         raise ValueError('Notes cannot be closed! Remove instead.')
@@ -22,21 +19,14 @@ class NotesInstrument(InstrumentBase):
         return True
     def start(self):
         return True
-    def measure(self):
+    def next(self)->dict:
         return {'text':self.text}
-    def next(self):
-        return self.measure()
     def card_html(self, iid: str, type_name: str = 'notes') -> str:
         return f"""
             <h3>{type_name} <small>({iid})</small></h3>
-            <p>Status: <span class=\"status\">{self.status}</span></p>
-            <p>IDN: <span class=\"idn\">{self.idn}</span></p>
-            <div class=\"device-controls\">\n      <button class=\"open\">Open</button>\n      <button class=\"close\">Close</button>\n      <button class=\"remove\">Remove</button>\n    </div>
+            <button class=\"remove\">Remove</button>
             <div class=\"grid\">
-            <div class=\"col\" id=\"{iid}-notes\">
-                <h4>Notes</h4>
                 <label>Text:</label>
-                <textarea data-key=\"text\" rows=\"8\" style=\"width:100%\">{self.text}</textarea>
-            </div>
+                <textarea data-key=\"text\" rows=\"4\" style=\"width:100%\">{self.text}</textarea>
             </div>
             """
